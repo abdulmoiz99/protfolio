0b45d7e59b85cb6a8ce432dbf5ae449128d9b2e7
Abdul Moiz <moiza0799@gmail.com>
2024-10-16 17:36:19 -0500

    Code changes

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -1,21 +1,14 @@
 import { Divider, Layout, Card, Button, Input } from 'antd';
 import { UserOutlined, EyeInvisibleOutlined, EyeTwoTone, KeyOutlined, GoogleOutlined, GithubOutlined } from '@ant-design/icons';
 
-const { Header, Footer, Content } = Layout;
+const { Content } = Layout;
 
-const headerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  padding: '16px 0',
-  color: '#fff',
-  fontSize: '20px',
-};
 
 const contentStyle: React.CSSProperties = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   minHeight: '80vh',
-  backgroundColor: '#f0f2f5',
 };
 
 const cardStyle: React.CSSProperties = {
@@ -25,47 +18,37 @@ const cardStyle: React.CSSProperties = {
   borderRadius: '8px',
 };
 
-const footerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  backgroundColor: '#001529',
-  color: '#fff',
-  padding: '12px 0',
-};
 
 export default function Login() {
   return (
-    <Layout>
-      <Header style={headerStyle}>Welcome to My App</Header>
-      <Content style={contentStyle}>
-        <Card title="Sign In" bordered={false} style={cardStyle}>
-          <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
-          <br />
-          <br />
-          <Input.Password
-            prefix={<KeyOutlined />}
-            size="large"
-            placeholder="Password"
-            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          />
-          <br />
-          <br />
-          <Button type="primary" block>
-            Login
-          </Button>
-          <Divider plain>or</Divider>
-          <Button block>
-            <GoogleOutlined />
-            Sign In With Google
-          </Button>
-          <br />
-          <br />
-          <Button block>
-            <GithubOutlined />
-            Sign In With Github
-          </Button>
-        </Card>
-      </Content>
-      <Footer style={footerStyle}>© 2024 My App. All rights reserved.</Footer>
-    </Layout>
+    <Content style={contentStyle}>
+      <Card title="Sign In" bordered={false} style={cardStyle}>
+        <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
+        <br />
+        <br />
+        <Input.Password
+          prefix={<KeyOutlined />}
+          size="large"
+          placeholder="Password"
+          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+        />
+        <br />
+        <br />
+        <Button type="primary" block>
+          Login
+        </Button>
+        <Divider plain>or</Divider>
+        <Button block>
+          <GoogleOutlined />
+          Sign In With Google
+        </Button>
+        <br />
+        <br />
+        <Button block>
+          <GithubOutlined />
+          Sign In With Github
+        </Button>
+      </Card>
+    </Content>
   );
 }
