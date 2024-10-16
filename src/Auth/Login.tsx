6e9a92e5086c94cbd66a316470b633e31e7e03f6
Abdul Moiz <moiza0799@gmail.com>
2024-10-16 17:40:21 -0500

    Separated css

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -1,49 +1,35 @@
+import React from 'react';
 import { Divider, Layout, Card, Button, Input } from 'antd';
 import { UserOutlined, EyeInvisibleOutlined, EyeTwoTone, KeyOutlined, GoogleOutlined, GithubOutlined } from '@ant-design/icons';
+import './Login.css';
 
 const { Content } = Layout;
 
-
-const contentStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  minHeight: '80vh',
-};
-
-const cardStyle: React.CSSProperties = {
-  width: 350,
-  textAlign: 'center',
-  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-  borderRadius: '8px',
-};
-
-
 export default function Login() {
   return (
-    <Content style={contentStyle}>
-      <Card title="Sign In" bordered={false} style={cardStyle}>
-        <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
-        <br />
-        <br />
+    <Content className="content">
+      <Card title="Sign In" bordered={false} className="card">
+        <Input
+          size="large"
+          placeholder="Username"
+          prefix={<UserOutlined />}
+          className="input"
+        />
         <Input.Password
           prefix={<KeyOutlined />}
           size="large"
           placeholder="Password"
           iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          className="input"
         />
-        <br />
-        <br />
-        <Button type="primary" block>
+        <Button type="primary" block className="primary-button">
           Login
         </Button>
         <Divider plain>or</Divider>
-        <Button block>
+        <Button block className="input">
           <GoogleOutlined />
           Sign In With Google
         </Button>
-        <br />
-        <br />
         <Button block>
           <GithubOutlined />
           Sign In With Github
